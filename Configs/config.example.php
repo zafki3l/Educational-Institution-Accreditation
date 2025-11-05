960e24c97487ae84f00daae79e71a33b64d6e489
zafki3l <phamdinhthai2005@gmail.com>
2025-11-06 01:38:48 +0700

    add config.example

---
+++ Configs/config.example.php
@@ -0,0 +1,14 @@
+<?php
+
+// Path configuration
+$rootFolder = basename(dirname(__DIR__));
+define('PROJECT_NAME', $rootFolder);
+define('BASE_PATH', dirname(__DIR__));
+define('VIEW_PATH', BASE_PATH . '/app/Views/');
+define('ROOT_PATH', __DIR__);
+
+// Database configuration
+define('DB_SERVER', 'YOUR_SERVER');
+define('DB_USER', 'YOUR_PASSWORD');
+define('DB_PASSWORD', 'YOUR_PASSWORD');
+define('DB_DATABASE', 'YOUR_DATABASE');
