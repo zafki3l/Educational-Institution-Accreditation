c4cc0f7b140903fea11cd3457a7bdaba8b9c966c
zafki3l <phamdinhthai2005@gmail.com>
2025-11-11 20:44:58 +0700

    fix Department

--- App/Database/Models/Department.php
+++ App/Database/Models/Department.php
@@ -4,15 +4,8 @@ namespace App\Database\Models;
 
 class Department
 {
-    public const DEPARTMENT_FINANCIAL = 1;
-
     private int $id;
     private string $name;
-
-    public static function ensureDepartmentIsFinancial(int $department_id)
-    {
-        return $department_id === self::DEPARTMENT_FINANCIAL;
-    }
 
     public function getId(): int {return $this->id;}
 
