43ba5471d71a93f39f9923cb92a5e0d804565210
Sota <sota.edit@gmail.com>
2025-11-29 16:50:04 +0700

    Css: 1/2 admin dashboard

--- App/Views/admin/dashboard.php
+++ App/Views/admin/dashboard.php
@@ -19,7 +19,7 @@
         <div class="staff-dashboard">
             <div class="dashboard-grid">
                 <!-- Staff Information Section -->
-                <section class="card staff-info-card">
+                <div class="card staff-info-card">
                     <header class="card-header">
                         <h3>Thông tin quản trị</h3>
                         <i class="fa-solid fa-user-circle fa-4x" aria-hidden="true"></i>
@@ -32,72 +32,75 @@
                             <div class="role-lable"><p><strong>Role:</strong><div class="role"><?= htmlspecialchars('Admin') ?></div></p></div>
                         </div>
                     </div>
-                </section>
+                </div>
 
-                <div class="system-overview">
+                <div class="card system-overview">
                     <h3>Tổng quan hệ thống</h3>
-                    <div class="criteria-box">
-                        <img src="/<?= PROJECT_NAME ?>/public/images/Check square.png" alt="">
+                    <div class="container-box">
+                        <a href="/<?= PROJECT_NAME ?>/admin/criterias"><div class="box criteria-box">
+                            <img src="/<?= PROJECT_NAME ?>/public/images/Check square.png" alt="">
+                            <p>100</p>
+                            <p>Tiêu chí</p>
+                        </div></a>
+                        <a href="/<?= PROJECT_NAME ?>/admin/users"><div class="box user-box">
+                            <img src="/<?= PROJECT_NAME ?>/public/images/vector.png" alt="">
+                            <p>40</p>
+                            <p>Người dùng</p>
+                        </div></a>
+                        <a href="/<?= PROJECT_NAME ?>/admin/standards"><div class="box evidence-box">
+                            <img src="/<?= PROJECT_NAME ?>/public/images/frame.png" alt="">
+                            <p>2000</p>
+                            <p>Minh chứng</p>
+                        </div></a>
+                        <a href="/<?= PROJECT_NAME ?>/admin/milestones"><div class="box milestone-box">
+                            <img src="/<?= PROJECT_NAME ?>/public/images/marker.png" alt="">
+                            <p>1000</p>
+                            <p>Mốc đánh giá</p>
+                        </div></a>
                     </div>
                 </div>
 
-                <section class="card inventory-card">
-                    <header class="card-header">
-                        <h3>Tiêu chuẩn, Tiêu chí, Mốc đánh giá</h3>
-                    </header>
-                    <div class="card-body">
-                        <div class="management-links">
-                            <a class="manage-link" href="/<?= PROJECT_NAME ?>/admin/standards">
-                                <i class="fa-solid fa-tags"></i>
-                                Cập nhật tiêu chuẩn
-                            </a>
-                            <a class="manage-link" href="/<?= PROJECT_NAME ?>/admin/criterias">
-                                <i class="fa-solid fa-tags"></i>
-                                Cập nhật tiêu chí
-                            </a>
-
-                            <a class="manage-link" href="/<?= PROJECT_NAME ?>/admin/milestones">
-                                <i class="fa-solid fa-tags"></i>
-                                Cập nhật mốc đánh giá
-                            </a>
-                        </div>
+                <div class="card management">
+                    <h3>Quản lý tiêu chí</h3>
+                    <div class="list evidence-list">
+                        <p>Danh sách tiêu chuẩn</p>
+                        <p>10 tiêu chuẩn</p>
                     </div>
-                </section>
+                    <div class="list criteria-list">
+                        <p>Danh sách tiêu chí</p>
+                        <p>2 tiêu chí</p>
+                    </div>
+                    <div class="list milestone-list">
+                        <p>Mốc đánh giá</p>
+                        <p>2 mốc</p>
+                    </div>
+                </div>
 
-                <section class="card orders-card">
+                <div class="card management">
+                    <h3>Quản lý người dùng</h3>
+                    <div class="list">
+                        <p>Quản trị viên</p>
+                        <p>1 Quản trị viên</p>
+                    </div>
+                    <div class="list">
+                        <p>Nhân viên</p>
+                        <p>200 Nhân viên</p>
+                    </div>
+                    <div class="list add-list">
+                        <a href="/<?= PROJECT_NAME ?>/admin/users/create">+ Thêm người dùng</a>
+                    </div>
+                </div>
+
+                <!-- Chart Report Section -->
+                <section class="card chart-card">
                     <header class="card-header">
-                        <h3>Quản lý người dùng</h3>
+                        <h3>Thống kê</h3>
                     </header>
-                    <div class="card-body">
-                        <div class="management-links">
-                            <a class="manage-link" href="/<?= PROJECT_NAME ?>/admin/users">
-                                <i class="fa-solid fa-tags"></i>
-                                Danh sách người dùng
-                            </a>
-
-                            <a class="manage-link" href="/<?= PROJECT_NAME ?>/admin/roles">
-                                <i class="fa-solid fa-tags"></i>
-                                Cập nhật quyền
-                            </a>
-
-                            <a class="manage-link" href="/<?= PROJECT_NAME ?>/admin/departments">
-                                <i class="fa-solid fa-tags"></i>
-                                Quản lý phòng ban
-                            </a>
-                        </div>
+                    <div class="card-body chart-body">
+                        <canvas id="myChart" aria-label="Sales chart"></canvas>
                     </div>
                 </section>
             </div>
-
-            <!-- Chart Report Section -->
-            <section class="card chart-card">
-                <header class="card-header">
-                    <h3>Thống kê</h3>
-                </header>
-                <div class="card-body chart-body">
-                    <canvas id="myChart" aria-label="Sales chart"></canvas>
-                </div>
-            </section>
         </div>
     </div>
 
