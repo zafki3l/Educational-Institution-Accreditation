a0a1c9ee50d6ffa9d57f8c4ae1e56a20dc790027
zafki3l <phamdinhthai2005@gmail.com>
2025-12-30 12:00:33 +0700

    add EvidenceRepositoryInterface

---
+++ App/Business/Ports/EvidenceRepositoryInterface.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Business\Ports;
+
+interface EvidenceRepositoryInterface
+{
+    public function all(int $start_from, int $result_per_page): array;
+
+    public function filter(int $start_from, int $result_per_page, array $filter): array;
+
+    public function countAll(): int;
+
+    public function create(array $evidence): int;
+
+    public function linkMinestoneToEvidence(string $evidence_id, string $milestone_id): int;
+
+    public function findById(string $evidence_id): array;
+
+    public function evidenceManyToManyMilestone(string $evidence_id): array;
+
+    public function evidenceWithoutMilestone(): array;
+
+    public function updateById(string $id, array $evidence): string;
+
+    public function deleteById(string $evidence_id): int;
+
+    public function search(string $search, int $start_from, int $result_per_page): array;
+
+    public function countSearch(string $search): int;
+}
